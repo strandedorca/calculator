ace5c98faeba0d66f1a13a1258c19667e878fd89
Stranded Orca <bestofmau@gmail.com>
2023-03-30 22:11:22 +0700

    Add script

--- javascript.js
+++ javascript.js
@@ -1,70 +1,77 @@
 /* VARIABLES */
-let num1 = num2 = op = 0;
+let num1 = num2 = op = "";
 
 /* DISPLAY */
+/* Display 0 if there's no value */
 const display = document.getElementById("display");
 function populate(value) {
-    if (value == "ಠ_ಠ?") {
-        display.textContent = value;
+    if (value == "") {
+        display.textContent = "0";
     } else {
         display.textContent = value;
     }
 }
 
 /* OPERATE */
-function operate(op, num1 = 0, num2 = 0) {
+/* Basic mathematic operations, including the division by 0 case */
+function operate(op, num1 = "", num2 = "") {
+    num1 = Number(num1);
+    num2 = Number(num2);
     switch (op) {
         case '+':
             return num1 + num2;
         case '-':
             return num1 - num2;
         case '*':
-            if (num2 == 0) num2 = 1;
+            // if (num2 == "") num2 = 1;
             return num1 * num2;
         case '/':
             if (num2 == 0) {
                 return "ಠ_ಠ?";
             }
-            return num1 / num2;
+            return Math.round((num1 / num2) * 100) / 100;
     }
 }
 
 /* DOM */
 /* Number buttons */
+let decimalUsed = false;
+/* Another way to assign handler to multiple elements: assign to its parent */
 const numbers = document.querySelector("#numbers");
 numbers.addEventListener('click', function(event) {
+    /* If not a button -> not interested */
     if (event.target.tagName == 'BUTTON') {
-        if (op == 0) {
-            num1 += event.target.textContent;
-            num1 = Number(num1);
-            populate(num1);
-            console.log(`First input: ${num1}.`);
-        } else {
-            num2 += event.target.textContent;
-            num2 = Number(num2);
-            populate(num2);
-            console.log(`First input: ${num1}.\nSecond input: ${num2}.`);
+        if (event.target.id != "decimal" || (event.target.id == "decimal" && decimalUsed == false)) {
+            console.log(event.target.id);
+            console.log(decimalUsed);
+            /* Check if the decimal point is used */
+            if (event.target.id == "decimal") {
+                decimalUsed = true;
+            }
+            if (op == "") {
+                /* event.target is the element that is clicked */
+                num1 += event.target.textContent;
+                populate(num1);
+                console.log(`First input: ${num1}.`);
+            } else {
+                num2 += event.target.textContent;
+                populate(num2);
+                console.log(`First input: ${num1}.\nSecond input: ${num2}.`);
+            }
         }
     }
-})
-
-/* Decimal button */
-let decimalUsed = false;
-const decimal = document.getElementById("decimal");
-decimal.addEventListener('click', function(event) {
-    if (decimalUsed == false) decimalUsed = true;
 })
 
 /* Operator buttons */
 const ops = document.querySelectorAll('.operators');
 for (let button of ops) {
     button.addEventListener('click', function(event) {
-        if (num2 != 0) {
+        if (num2 != "") {
             let result = operate(op, num1, num2);
             populate(result);
             console.log(result)
             num1 = result;
-            num2 = 0;
+            num2 = "";
             decimalUsed = false;
         } else {
             op = event.target.value;    
@@ -73,30 +80,46 @@ for (let button of ops) {
     })
 }
 
-/* = button */
+/* Equal button */
 const equal = document.getElementById("equal");
 equal.addEventListener('click', () => {
-    if (op != 0) {
+    if (op != "") {
+        if (num2 == "") {
+            /* Do nothing if the second operant is not provided */
+            return;
+        }
         let result = operate(op, num1, num2);
         populate(result);
         console.log(`${num1} ${op} ${num2}`);
         console.log(result);
         num1 = result;
-        num2 = 0;
+        num2 = "";
         decimalUsed = false;
     }
 })
 
-/* Clear buttons */
+/* Clear everythng */
 const ac = document.getElementById("ac");
 ac.addEventListener('click', () => {
-    num1 = num2 = op = 0;
-    populate(0);
+    num1 = num2 = op = "";
+    populate("");
     console.clear();
+    decimalUsed = false;
 })
+
+/* Clear the last input */
 const c = document.getElementById("c");
-c.addEventListener('click', () => {
-    alert("last number inpute cleared!")
+c.addEventListener('click', function(event) {
+    if (op == "") {
+        num1 = num1.slice(0, num1.length - 1);
+        populate(num1);
+        console.log(`First input: ${num1}.`);
+    } else {
+        num2 = num2.slice(0, num2.length - 1);
+        populate(num2);
+        console.log(`First input: ${num1}.\nSecond input: ${num2}.`);
+    }
+    console.log("last input cleared")
 })
 
 console.log("Hello Check Check")
